c1b5046ea9cc85da3bee847fde3a20738ab66ce6
Michal Wisniewski <mihuwis@gmail.com>
2018-09-18 11:57:50 +0200

    Add Parser class

---
+++ onlineshop/src/main/java/com/codecool/onlineshop/controlers/ProductParser.java
@@ -0,0 +1,20 @@
+package com.codecool.onlineshop.controlers;
+
+import com.codecool.onlineshop.model.Product;
+
+public class ProductParser{
+    String fileName;
+
+    public ProductParser(String fileName){
+
+    }
+
+    public void loadProducts(){
+
+    }
+
+    public void addProduct(Product product){
+
+    }
+
+}
