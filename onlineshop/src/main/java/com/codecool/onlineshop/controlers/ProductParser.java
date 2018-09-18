49544305b03e45e467dec763d6dc9ff0a68626d1
Michal Wisniewski <mihuwis@gmail.com>
2018-09-18 15:38:55 +0200

    Add writeXMLFile class
    class is under construction

--- onlineshop/src/main/java/com/codecool/onlineshop/controlers/ProductParser.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/controlers/ProductParser.java
@@ -1,6 +1,14 @@
 package com.codecool.onlineshop.controlers;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
 import com.codecool.onlineshop.model.Product;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class ProductParser{
     String fileName;
@@ -15,6 +23,29 @@ public class ProductParser{
 
     public void addProduct(Product product){
 
+    }
+
+    public void writeXMLFle(){
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBulider = docFactory.newDocumentBuilder();
+
+            Document doc = docBulider.newDocument();
+            Element onelineShop = doc.createElement("OnlineShop");
+            doc.appendChild(onelineShop);
+
+            
+
+
+
+
+
+        } catch (ParserConfigurationException pce){
+            pce.printStackTrace();
+
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
     }
 
 }
