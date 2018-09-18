f5f5fc3e6f235828ac4f29d648cc48c7d6778316
crabvendor <st.wyrobek@gmail.com>
2018-09-18 15:03:14 +0200

    Implement Product and ProductCategory models

--- onlineshop/src/main/java/com/codecool/onlineshop/model/Product.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/model/Product.java
@@ -1,5 +1,64 @@
 package com.codecool.onlineshop.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Product {
 
+    private static List<Product> productList = new ArrayList<>();
+    private String name;
+    private Float defaultPrice;
+    private ProductCategory productCategory;
+    private Integer ID;
+
+    public Product(){}
+
+    public Product(String name, Float defaultPrice, ProductCategory productCategory) {
+        this.name = name;
+        this.defaultPrice = defaultPrice;
+        this.productCategory = productCategory;
+        this.ID = generateID();
+        productList.add(this);
+    }
+
+    private Integer generateID() {
+        UUID uuid = UUID.randomUUID();
+        return (Integer)uuid.toString();
+    }
+
+    public ArrayList<Product> getAllProducts() {
+        return productList;
+    }
+
+    public ArrayList<Product> getAllProductsBy(ProductCategory productCategory) {
+        List<Product> productsOfCategory = new ArrayList<>();
+
+        for (Product product : productList) {
+            if (isProductTypeOfCategory(product, productCategory)) {
+                productsOfCategory.add(product);
+            }
+        }
+        return productsOfCategory;
+    }
+
+    public Integer getProductId() {
+        return ID;
+    }
+
+    public Integer getProductCategoryId() {
+        return productCategory.getID();
+    }
+
+    public String getCategoryName() {
+        return productCategory.getCategoryName();
+    }
+
+    private boolean isProductTypeOfCategory(Product product, ProductCategory productCategory) {
+        return product.getProductCategoryId() == productCategory.getID();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID:%d,name:%s,defaultprice:%.2d", ID, name, defaultPrice);
+    }
 }
--- onlineshop/src/main/java/com/codecool/onlineshop/model/ProductCategory.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/model/ProductCategory.java
@@ -1,5 +1,27 @@
 package com.codecool.onlineshop.model;
 
-public class ProductCategory{
+public class ProductCategory {
     
+    String name;
+    Integer ID;
+    
+    public ProductCategory() {}
+
+    public ProductCategory(String name) {
+        this.name = name;
+        this.ID = generateID();
+    }
+
+    Integer generateID() {
+        UUID uuid = UUID.randomUUID();
+        return (Integer)uuid.toString();
+    }
+
+    public String getCategoryName() {
+        return name;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
 }
