532ae37b581b716da3a451ff2501874c1f5d0646
Marta Zając <xmartza1@gmail.com>
2018-09-18 14:47:37 +0200

    Implement methods loadProductsFromFile, saveProductsTo, showTableOfProducts, newOrder, exit

--- onlineshop/src/main/java/com/codecool/onlineshop/controlers/ShopController.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/controlers/ShopController.java
@@ -8,32 +8,34 @@ public class ShopController{
     private ProductController product;
     private BasketController basket;
     private OrderController order;
+    private ProductParser parser;
     private View view;
 
     public ShopController(){
         product = new ProductController();
         basket = new BasketController();
-        order = new OrderController();
+        parser = new ProductParser(fileName);
         view = new View();
     }
 
     public void loadProductsFromFile(String fileName){
+        parser.loadProducts(fileName);
 
     }
 
     public void saveProductsTo(String fileName){
-
+        parser.serializeProductsToFile(fileName);
     }
 
     public void showTableOfProducts(){
-
+        view.showTableOfProducts();
     }
 
     public void  newOrder(){
-
+        order = new OrderController();
     }
 
     public void exit(){
-
+        System.exit(0);
     }
 }
