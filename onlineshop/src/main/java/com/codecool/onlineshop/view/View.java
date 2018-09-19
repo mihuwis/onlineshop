a8246fcb14e16f6bfd4e982bf4c08121a12681fe
crabvendor <st.wyrobek@gmail.com>
2018-09-19 14:21:22 +0200

    Implement printing table of products

--- onlineshop/src/main/java/com/codecool/onlineshop/view/View.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/view/View.java
@@ -1,5 +1,13 @@
 package com.codecool.onlineshop.view;
 
+import main.java.com.codecool.onlineshop.view.TableBuilder;
+
 public class View {
     
+    TableBuilder tableBuilder = new TableBuilder(products);
+
+    public void printProductsTable() {
+        System.out.println(tableBuilder.getProductsTable());
+    }
+
 }
