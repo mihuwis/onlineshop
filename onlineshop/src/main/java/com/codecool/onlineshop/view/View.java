ef56cd5dd519080f653f573bd8783764d57cbdbe
crabvendor <st.wyrobek@gmail.com>
2018-09-19 14:54:43 +0200

    Implement printing messages and Menus

--- onlineshop/src/main/java/com/codecool/onlineshop/view/View.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/view/View.java
@@ -6,8 +6,56 @@ public class View {
     
     TableBuilder tableBuilder = new TableBuilder(products);
 
+    public enum Message {
+        ASK_FOR_NAME("Enter product name: "),
+        ASK_FOR_ID("Enter product ID: ");
+
+        String message;
+
+        private Message(String message) {
+            this.message = message;
+        }
+
+        private String getMessage() {
+            return message;
+        }
+    }
+
+    public enum Menu {
+        MAIN_MENU(new String[] {"TEMP", "TEMP", "EXIT"}, "TEMPTEMPTEMP");
+
+        String[] menuOptions;
+        String title;
+
+        private Menu(String[] menuOptions, String title) {
+            this.menuOptions = menuOptions;
+            this.title = title;
+        }
+
+        private String[] getMenuOptions() {
+            return menuOptions;
+        }
+
+        private String getTitle() {
+            return title;
+        }
+    }
+
     public void printProductsTable() {
         System.out.println(tableBuilder.getProductsTable());
     }
 
+    public void printMessage(Message message) {
+        System.out.println(message.getMessage());
+    }
+
+    public void printMenu(Menu menu) {
+        String[] mainMenu = menu.getMenuOptions();
+        String gameTitle = menu.getTitle();
+
+        System.out.printf("%s\n", gameTitle);
+        for (int i=0; i<mainMenu.length; i++) {
+            System.out.printf("\t%d) %s\n", i+1, mainMenu[i]);
+        }
+    }
 }
