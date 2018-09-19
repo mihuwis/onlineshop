f028a1cc0a613dc3035674098f9430cdf162507d
crabvendor <st.wyrobek@gmail.com>
2018-09-19 14:19:53 +0200

    Implement ProductController Class

--- onlineshop/src/main/java/com/codecool/onlineshop/controlers/ProductController.java
+++ onlineshop/src/main/java/com/codecool/onlineshop/controlers/ProductController.java
@@ -1,5 +1,30 @@
 package com.codecool.onlineshop.controlers;
 
-public class ProductController{
+import com.codecool.onlineshop.model.Product;
+import com.codecool.onlineshop.model.ProductCategory;
+
+public class ProductController {
     
+    private Product product = new Product();
+    private ProductParser parser = new Parser();
+
+    public List getAllProducts() {
+        product.getAllProducts();
+    }
+
+    public List getAllProductsBy(ProductCategory productCategory) {
+        product.getAllProductsBy(productCategory);
+    }
+
+    public void addProduct(String name, Float defaultPrice, ProductCategory productCategory) {
+        Product newProduct = new Product(name, defaultPrice, productCategory);
+    }
+
+    public void loadProducts() {
+
+    }
+
+    public void saveProducts() {
+
+    }
 }
